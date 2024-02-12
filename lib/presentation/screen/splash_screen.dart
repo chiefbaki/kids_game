ce08ef9dfba363b1f15d9cff2c29a1af77104203
IslamKurbanov <ik509331@gmail.com>
2024-02-12 18:06:48 +0600

    fixed

--- lib/presentation/screen/splash_screen.dart
+++ lib/presentation/screen/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:kids_game/core/consts/app_color.dart';
 import 'package:kids_game/presentation/screen/choose_character_screen.dart';
 import 'package:kids_game/presentation/screen/profile_screen.dart';
 import 'package:kids_game/resources/resources.dart';
@@ -13,10 +14,7 @@ class SplashScreen extends StatelessWidget {
       gradient: const LinearGradient(
         begin: Alignment.topCenter,
         end: Alignment.bottomCenter,
-        colors: [
-          Colors.lightBlue,
-          Colors.blue,
-        ],
+        colors: AppColors.bgColor,
       ),
       childWidget: SizedBox(
         height: 400,
