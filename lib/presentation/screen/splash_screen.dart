5823d8f765ad5f7abd7edef6af1fbe3b626624b2
IslamKurbanov <ik509331@gmail.com>
2024-02-12 18:14:16 +0600

    added navigator

--- lib/presentation/screen/splash_screen.dart
+++ lib/presentation/screen/splash_screen.dart
@@ -2,7 +2,6 @@ import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart
 import 'package:flutter/material.dart';
 import 'package:kids_game/core/consts/app_color.dart';
 import 'package:kids_game/presentation/screen/choose_character_screen.dart';
-import 'package:kids_game/presentation/screen/profile_screen.dart';
 import 'package:kids_game/resources/resources.dart';
 
 class SplashScreen extends StatelessWidget {
